1fbce53afb813167d7b7073f8ebc404fb5727b9c
Bank5807 <tanat_wip@cmu.ac.th>
2020-01-28 16:36:57 +0700

    lab9 v1

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -1,4 +1,22 @@
-//cout << "-------------------- SOTUS ---------------------";
-//cout << textline;
-//cout << "-------------------- SOTUS ---------------------";
-//Don't forget to change cout to output filestream
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream source("cheerbook.txt");
+    string textline, cop;
+    while (getline(source,textline)){
+        cop += textline;
+        cop += "\n";
+    }
+   source.close();
+
+   ofstream dest("cheerbook_copy.txt");
+   dest << "--------------------SOTUS-----------------------\n";
+   dest << cop;
+   dest << "--------------------SOTUS-----------------------\n";
+   dest.close();
+    return 0;
+}
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,24 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main ()
+{
+    int count = 0;
+    float sum = 0, sum2 = 0, M = 0;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source, textline)){
+        sum += atof(textline.c_str());
+        sum2 += pow(atof(textline.c_str()), 2);
+        count++;
+    }
+    M = (1.0/count)*sum;
+    cout << "number of data = " << count << endl;
+    cout << "Mean = " << M << endl;
+    cout << "Standard deviation = " << sqrt(((1.0/count)*sum2) - pow(M, 2)) << endl;
+    source.close();
+    return 0;
+}
